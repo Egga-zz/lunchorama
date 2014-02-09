9bd14a1f4296d21f92d8f9dbe7b3ba123f1ca3c3
Jan Hartung <egga@cpan.org>
2014-02-09 16:16:20 -0500

    test don't start server, add endpoint

--- places/controller.js
+++ places/controller.js
@@ -8,17 +8,28 @@ var Place = mongoose.model('places', placesSchema);
 
 exports.setup = function( app ) {
     app.get('/places', list);
-
+    app.get("/places/:id", read);
 };
 
 function list (req, res){
-
-    Place.find(function (err, places) {
-        if (err) {
-            console.log("couldn't list: " + err);
-        }
-
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify(places));
+    Place.find( function (err, places) {
+        answer(err, "list", res, places);
     });
 };
+
+function read ( req, res ) {
+    var id = req.params.id;
+    Place.findOne({ "_id": id}, function ( err, place ) {
+        answer(err, "read", res, place);
+    });
+}
+
+function answer ( err, verb, res, data ) {
+    if (err) {
+        console.log("couldn't " + verb + ": " + err);
+        throw err;
+    }
+
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(data));
+}
--- places/integration_test.js
+++ places/integration_test.js
@@ -7,16 +7,33 @@ var mongoose = require('mongoose'),
     entity = require("./entity"),
     Place = mongoose.model('Place');
 
+
+request = request('http://localhost:8080');
+
 describe('/places', function(){
     it('returns a list of all places', function(){
-        require("../bin/app").startApp(function ( app ) {
-            request(app)
-                .get('/places')
-                .expect('Content-Type', /json/)
-                .expect(200)
-                .end(function(err, res){
-                    if (err) throw err;
-                });
-        });
+
+        request.get('/places')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end(function(err, res){
+                if (err) throw err;
+            });
+    });
+});
+describe('/places/:id', function(){
+    it('returns a specific place with given :id', function(){
+
+        request.get('/places/52f6411d22ecf0ad715d5108')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .expect( {
+                "name":"Best Rest",
+                "_id":"52f6411d22ecf0ad715d5108",
+                "__v":0
+            })
+            .end(function(err, res){
+                if (err) throw err;
+            });
     });
 });
